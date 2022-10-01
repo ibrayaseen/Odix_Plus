6b3495b48ea99bfeab64617d17c655240e9ec53c
ibrayaseen <34011226+ibrayaseen@users.noreply.github.com>
2022-10-01 23:35:41 +0300

    dcsdc

--- install.bat
+++ install.bat
@@ -1,6 +1,12 @@
-git clone https://github.com/ibrayaseen/Odix_Plus.git Odix_Plus
-if "%var%" == "y" (
-  @echo some commands went here
+
+if exist Odix_Plus_test (
+	  
+	  git config --global --add safe.directory D:/Odix_Plus_test
+	  cd D:\Odix_Plus_test
+     git pull https://github.com/ibrayaseen/Odix_Plus.git
 ) else (
-  @echo different commands went here
-}
+    git clone https://github.com/ibrayaseen/Odix_Plus.git Odix_Plus_test
+)
+pause
+
+Odixpro@123
